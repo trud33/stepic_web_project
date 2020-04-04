0af3808b46de55e6accd0b31b86d2bf6560136bc
Igor Bohdan <igor3407.b@gmail.com>
2020-04-04 15:16:58 +0300

    +5

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
-sudo rm /etc/nginx/sites-enabled/default
+#!/bin/bash
+
+if [ -f /etc/nginx/sites-enabled/default ]; then
+  sudo rm /etc/nginx/sites-enabled/default
+fi
+
+# Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart

e5fe4415f65ab5c487529b0215f7417f6ae3e373
Igor Bohdan <igor3407.b@gmail.com>
2020-04-06 14:37:57 +0300

    r

--- init.sh
+++ init.sh
@@ -5,10 +5,15 @@ if [ -f /etc/nginx/sites-enabled/default ]; then
 fi
 
 # Nginx
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
 sudo /etc/init.d/nginx restart
 
 # Gunicorn
 sudo ln -sf /home/box/web/etc/gunicorn_hello.conf /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
+
+# MySQL
+sudo service mysql restart
+sudo mysql -uroot -e "CREATE DATABASE qa CHARACTER SET utf8 COLLATE utf8_general_ci;"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON qa.* TO 'qa_user'@'localhost' IDENTIFIED BY '123456789';"
